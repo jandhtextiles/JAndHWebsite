f1b55c20932280fc74d42b79bc9648099115afe6
rohanpatel4846 <rohanpatel4846@gmail.com>
2025-01-17 23:22:31 +0530

    Fixed Footer

--- 971.4fab3c570317cde9.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkJAndHWebsite=self.webpackChunkJAndHWebsite||[]).push([[971],{1971:(E,d,a)=>{a.d(d,{vh:()=>O});var r=a(4650),c=a(445),l=a(6895),p=a(6287);let O=(()=>{class t{}return t.\u0275fac=function(e){return new(e||t)},t.\u0275mod=r.oAB({type:t}),t.\u0275inj=r.cJS({imports:[l.ez,p.T,c.vT]}),t})()}}]);
